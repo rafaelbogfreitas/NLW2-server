14828e87c1b59fb4217d9bc9133c56eef8d45a74
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-08-05 20:06:10 -0300

    Configure database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
 import express from "express";
 
+const app = express();
 
-console.log("Hello World ONe");
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  return res.json({ message: "HELLO WORLD!"})
+});
+
+app.listen(3333, () => console.log("Server running"))
