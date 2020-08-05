8ee92d899849cb9e785db7819d58494f58b65a91
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-08-05 20:32:23 -0300

    Finish classes table

--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex) {
     table.string("name").notNullable();
     table.string("avatar").notNullable();
     table.string("whatsapp").notNullable();
+    table.string("bio").notNullable();
   })
 }
 
