5390e58288355ebfd484833ed61033b0ff56a629
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-08-06 15:18:59 -0300

    Fix bug on connections migration

--- routes.ts
+++ routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import ClassesController from "./src/controllers/classesControllers";
+import ConnectionsControllers from "./src/controllers/ConnectionsControllers";
 const routes = express.Router();
 
 const classesControllers = new ClassesController;
+const connectionsControllers = new ConnectionsControllers;
 
-routes.post("/classes", classesControllers.create);
 routes.get("/classes", classesControllers.index);
+routes.post("/classes", classesControllers.create);
+
+routes.get("/connections", connectionsControllers.index);
+routes.post("/connections", connectionsControllers.create);
 
 export default routes;
---
+++ src/controllers/ConnectionsControllers.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from "express";
+import db from "../database/connection";
+export default class ConnectionsControllers {
+  async create(req: Request, res: Response) {
+    const { user_id } = req.body;
+
+    await db('connections').insert({
+      user_id,
+    });
+
+    return res.status(201).send();
+
+  }
+
+  async index(req: Request, res: Response) {
+
+  }
+}
